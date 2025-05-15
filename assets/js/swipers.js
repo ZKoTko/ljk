92d14739372719d56b21f8c28542989435a5b5f2
KoTko (Konstantin Bagdasarov) <75670213+ZKoTko@users.noreply.github.com>
2025-05-16 00:23:31 +0400

    sazzs

--- assets/js/swipers.js
+++ assets/js/swipers.js
@@ -21,7 +21,7 @@ var swiper = new Swiper('.cases-swiper', {
         768: {
             slidesPerView: 2,
         },
-        1024: {
+        1200: {
             slidesPerView: 3,
         },
         1624: {
@@ -43,7 +43,19 @@ var swiper = new Swiper('.story-swiper', {
       el: '.swiper-pagination',
       clickable: true,
     },
+    breakpoints: {
+        0: { // от 0 и выше
+            slidesPerView: 2,
+        },
+        768: { // от 768 и выше
+            slidesPerView: 3,
+        },
+        1200: { // от 1200 и выше
+            slidesPerView: 4,
+        }
+    }
 });
+
 
 var swiper = new Swiper('.auditoria-swiper', {
     slidesPerView: 3,
@@ -57,7 +69,16 @@ var swiper = new Swiper('.auditoria-swiper', {
       el: '.swiper-pagination',
       clickable: true,
     },
+    breakpoints: {
+        0: { // от 0 до 1199px
+            slidesPerView: 2,
+        },
+        1200: { // от 1200px и выше
+            slidesPerView: 3,
+        }
+    }
 });
+
 
 var swiper = new Swiper('.visualization-swiper', {
     slidesPerView: 'auto',
